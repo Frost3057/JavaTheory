b64330157dc51682463033c92504b522fdab4999
Frost3057 <amanbajpai5660@gmail.com>
2024-07-23 12:31:51 +0530

    TheoryCode

---
+++ src/eg3.java
@@ -0,0 +1,5 @@
+public class eg3 {
+    public static void main(String[] args) {
+        System.out.println(10==15);
+    }
+}
---
+++ src/eg5.java
@@ -0,0 +1,12 @@
+public class eg5 {
+    public static void main(String[] args) {
+        int time = 22;
+        if(time<10){
+            System.out.println("Good Morning.");
+        } else if (time<20) {
+            System.out.println("Good day");
+        } else{
+            System.out.println("Good Evening.");
+        }
+    }
+}
